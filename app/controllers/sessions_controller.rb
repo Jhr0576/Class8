699d7f4906fec6edbef6ce787c7ab737aaeb1a1b
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-05-20 19:22:20 +0000

    Save3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,22 @@ class SessionsController < ApplicationController
     end
 
     def create
+@user = User.find_by({ "email" => params["email"] })
 
-        
+if @user
+
+    if params["password"] == @user["password"]
+
+        redirect_to "/companies"
+
+else
+
+    redirect_to "/sessions/new"
+    end
+else
+    redirect_to "/sessions/new"
+
     end
 
-end
+
+
